94da4be589172c1dfee4abc8b08f5c3c1218254c
Synthex Development <104361414+SynthexDevelopment@users.noreply.github.com>
2023-09-01 18:53:16 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+--[[
+    COPYRIGHT (C) 2023 Synthex Development. ALL RIGHTS RESERVED.
+    DO NOT DISTRIBUTE WITHOUT PERMISSION.
+    Email: hhype.vibez@gmail.com
+]]
+
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Synthex Development'
+lua54 'yes'
+description 'This is a dvall script, used by WLD; it was recoded to give u a better example of coding!! Please, note that this is copyrighted and you cant steal this script'
+version 'v2023.08.30'
+
+client_script '**/client.lua'
+server_script '**/server.lua'
+
